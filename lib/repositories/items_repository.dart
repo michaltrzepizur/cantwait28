3f2b16a943a702627336a2bd489249a0d7726058
Michał Trzepizur <ramzes21246@gmail.com>
2024-10-30 19:35:33 +0100

    Add Details Page

--- lib/repositories/items_repository.dart
+++ lib/repositories/items_repository.dart
@@ -21,27 +21,32 @@ class ItemsRepository {
     });
   }
 
-
   Future<void> delete({required String id}) {
-   return FirebaseFirestore.instance
-          .collection('items')
-          .doc(id)
-          .delete();
+    return FirebaseFirestore.instance.collection('items').doc(id).delete();
   }
-Future<void> add(
-  String title, 
-  String imageURL, 
-  DateTime releaseDate,
+
+  Future<ItemModel> get({required String id}) async {
+    final doc =
+        await FirebaseFirestore.instance.collection('items').doc(id).get();
+    return ItemModel(
+      id: doc.id,
+      title: doc['title'],
+      imageURL: doc['image_url'],
+      relaseDate: (doc['release_date'] as Timestamp).toDate(),
+    );
+  }
+
+  Future<void> add(
+    String title,
+    String imageURL,
+    DateTime releaseDate,
   ) async {
-      await FirebaseFirestore.instance.collection('items').add(
-        {
-          'title': title,
-          'image_url': imageURL,
-          'release_date': releaseDate,
-        },
-      );
+    await FirebaseFirestore.instance.collection('items').add(
+      {
+        'title': title,
+        'image_url': imageURL,
+        'release_date': releaseDate,
+      },
+    );
   }
-
-
-
 }
